f367e57e68981711490bb583bee34dbe74fd32f7
ErandyR <rerandy15@gmail.com>
2017-03-23 22:40:28 +0000

    ingresando operadores

--- calculadora.js
+++ calculadora.js
@@ -9,3 +9,19 @@ function mostrarOperador(operador){
   numero += operador;
   document.getElementById("valorIngresado").value = numero;
 }
+
+function elegirOperador(){
+  var suma = document.getElementById("botonSuma").value;
+  var resta = document.getElementById("botonResta").value;
+  if (operador == suma){
+    resultado = Number(num1 + num2);
+    return resultado;
+  }
+  else if (operador == resta){
+    resultado = Number(num1 - num2);
+    return resultado
+  }
+}
+function mostrarResultado(){
+
+}
